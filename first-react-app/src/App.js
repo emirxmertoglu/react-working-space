6c7d5b4b56976b522d6a38f8c754d2fd683b32f2
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-05-26 16:26:52 +0300

    reacr create element

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,48 +1,40 @@
-import logo from "./logo.svg";
-import MyComponent from "./MyComponent";
-import styles from "./App.module.css";
-import { Title } from "./Title";
-import MyNav from "./MyNav";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import MyNav2 from "./MyNav2";
+import { createElement } from "react";
 import "./styles.scss";
 
 function App() {
-  return (
-    <>
-      <div className={styles.test}>
-        <h3>{process.env.NODE_ENV}</h3>
-        <p>{process.env.REACT_APP_API_URL}</p>
-        {process.env.NODE_ENV === "production" && (
-          <>
-            <span>{process.env.NODE_ENV} ortamindayiz</span>
-            <img src={logo} alt="logo" width="250" />
-          </>
-        )}
-      </div>
+  const todos = ["todo1", "todo2", "todo3"];
+  /*const Button = (props) => {
+    return <button>{props.text}</button>;
+  };*/
 
-      <MyComponent />
-
-      <Title>Lorem, ipsum dolor.</Title>
-      <Title theme="dark">Lorem, ipsum dolor.</Title>
-
-      <MyNav />
-
-      <MyNav2 />
-
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-
-      <div className="myDiv">
-        <p className="p-test">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus
-          laborum nisi mollitia. Distinctio, veniam. Officiis fuga, dignissimos
-          dicta dolore hic labore! Dolorum provident alias explicabo tempora
-          sit, aliquam sunt ipsum?
-        </p>
-      </div>
-    </>
+  /*const ul = createElement(
+    "ul",
+    { className: "todo-list" },
+    todos.map((todo) => createElement("li", { className: "todo" }, todo))
   );
+
+  return createElement(
+    "main",
+    {
+      className: "test",
+      id: "main",
+    },
+    ul
+  );*/
+
+  return (
+    <main className="test" id="main">
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li className="todo" key={index}>
+            {todo}
+          </li>
+        ))}
+      </ul>
+    </main>
+  ); // this is the same as the code above
+
+  // return <Button text="merhaba dunya" />;
 }
 
 export default App;
---
+++ first-react-app/src/Styles.js
@@ -0,0 +1,45 @@
+import MyNav2 from "./MyNav2";
+import logo from "./logo.svg";
+import MyComponent from "./MyComponent";
+import styles from "./App.module.css";
+import { Title } from "./Title";
+import MyNav from "./MyNav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+function Styles() {
+  <>
+    <div className={styles.test}>
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>{process.env.REACT_APP_API_URL}</p>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <span>{process.env.NODE_ENV} ortamindayiz</span>
+          <img src={logo} alt="logo" width="250" />
+        </>
+      )}
+    </div>
+
+    <MyComponent />
+
+    <Title>Lorem, ipsum dolor.</Title>
+    <Title theme="dark">Lorem, ipsum dolor.</Title>
+
+    <MyNav />
+
+    <MyNav2 />
+
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+    <div className="myDiv">
+      <p className="p-test">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus laborum
+        nisi mollitia. Distinctio, veniam. Officiis fuga, dignissimos dicta
+        dolore hic labore! Dolorum provident alias explicabo tempora sit,
+        aliquam sunt ipsum?
+      </p>
+    </div>
+  </>;
+}
+
+export default Styles;
