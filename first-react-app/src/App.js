55102c379722a648670a3a3d4799b25487007ba4
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-05-26 11:58:11 +0300

    module css

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.svg";
+import MyComponent from "./MyComponent";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div>
-      <h3>{process.env.NODE_ENV}</h3>
-      <p>{process.env.REACT_APP_API_URL}</p>
-      {process.env.NODE_ENV === "production" && (
-        <>
-          <span>{process.env.NODE_ENV} ortamindayiz</span>
-          <img src={logo} alt="logo" width="250" />
-        </>
-      )}
-    </div>
+    <>
+      <div className={styles.test}>
+        <h3>{process.env.NODE_ENV}</h3>
+        <p>{process.env.REACT_APP_API_URL}</p>
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <span>{process.env.NODE_ENV} ortamindayiz</span>
+            <img src={logo} alt="logo" width="250" />
+          </>
+        )}
+      </div>
+
+      <MyComponent />
+    </>
   );
 }
 
---
+++ first-react-app/src/MyComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyComponent() {
+  return <div className="test">MyComponent</div>;
+}
+
+export default MyComponent;
