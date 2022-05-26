d3852a2a63c2f4b3e48cf38e7eb3d022d3a4da93
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-05-26 11:47:26 +0300

    environment variables

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -3,7 +3,14 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div>
-      <img src={logo} alt="logo" width="250" />
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>{process.env.REACT_APP_API_URL}</p>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <span>{process.env.NODE_ENV} ortamindayiz</span>
+          <img src={logo} alt="logo" width="250" />
+        </>
+      )}
     </div>
   );
 }
