05e40e6157f427bb477e69904eed6b8911026c6a
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-06-19 00:46:14 +0300

    post lifecycle

--- lifecycle/src/components/Test.js
+++ lifecycle/src/components/Test.js
@@ -1,27 +1,39 @@
 import { useEffect, useState } from "react";
 
 function Test() {
-  const [count, setCount] = useState(0);
+  const [postID, setPostID] = useState(1);
+  const [post, setPost] = useState(null);
 
   useEffect(() => {
     console.log("component mounted");
+    let interval = setInterval(() => {
+      console.log("interval");
+    }, 1 * 1000);
     return () => {
+      clearInterval(interval);
+      console.log("interval cleared");
       console.log("component unmounted");
     };
   }, []);
 
-  useEffect(() => {
+  /* useEffect(() => {
     console.log("component updated");
-  });
+  }); */
 
   useEffect(() => {
-    console.log("count changed", count);
-  }, [count]);
+    console.log("postID changed", postID);
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postID}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, [postID]);
 
   return (
     <>
-      <h3>{count}</h3>
-      <button onClick={() => setCount((count) => count + 1)}>Increase</button>
+      <h3>{postID}</h3>
+      {post ? <pre>{JSON.stringify(post, null, 2)}</pre> : <p>Loading...</p>}
+      <button onClick={() => setPostID((postID) => postID + 1)}>
+        Next Post
+      </button>
       <hr />
       <div
         style={{
@@ -32,7 +44,7 @@ function Test() {
           marginTop: 20,
         }}
       >
-        Test
+        Test Component
       </div>
     </>
   );
