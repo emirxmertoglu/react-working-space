78412a75d3666cea3d1894c947e5a20119d4e053
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-06-21 21:55:16 +0300

    useRef, forwardRef

--- useref-forwardref/src/App.jsx
+++ useref-forwardref/src/App.jsx
@@ -1,7 +1,31 @@
+import { useRef, forwardRef } from "react";
+
+const Input = forwardRef((props, ref) => {
+  return <input type="text" ref={ref} {...props} />;
+});
+
 function App() {
+  const inputFocus = useRef();
+  const handleFocus = () => {
+    inputFocus.current.focus();
+  };
+
+  const componentInputFocus = useRef();
+  const handleComponentFocus = () => {
+    componentInputFocus.current.focus();
+  };
+
   return (
     <>
-      <div>emir</div>
+      <div>
+        <input type="text" ref={inputFocus} placeholder="html input" />
+        <button onClick={handleFocus}>Focus on input</button>
+      </div>
+      <hr />
+      <div>
+        <Input ref={componentInputFocus} placeholder="input component" />
+        <button onClick={handleComponentFocus}>Focus on input component</button>
+      </div>
     </>
   );
 }
