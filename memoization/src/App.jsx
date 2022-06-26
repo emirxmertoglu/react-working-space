3c795945bb1951361e2f86ee70f876e49ec5e519
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-06-26 23:27:33 +0300

    update: todo app

--- memoization/src/App.jsx
+++ memoization/src/App.jsx
@@ -1,5 +1,43 @@
+import { useReducer } from "react";
+import todoReducer from "./reducers/todoReducer";
+
 function App() {
-  return <></>;
+  const [state, dispatch] = useReducer(todoReducer, {
+    todos: [],
+    todo: "",
+  });
+
+  const handleChange = (e) => {
+    dispatch({
+      type: "SET_TODO",
+      value: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "ADD_TODO",
+      todo: state.todo,
+    });
+  };
+
+  return (
+    <>
+      <h1>Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={state.todo} onChange={handleChange} />
+        <button disabled={!state.todo} type="submit">
+          Add Todo
+        </button>
+      </form>
+      <ul>
+        {state.todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
