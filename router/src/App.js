954e07e3eceb1ac1bd5732a987045adbba7d8678
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-07-07 23:56:21 +0300

    edit: routes path

--- router/src/App.js
+++ router/src/App.js
@@ -1,47 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import BlogLayout from "./pages/blog";
-import Blog from "./pages/blog/Blog";
-import Categories from "./pages/blog/Categories";
-import Post from "./pages/blog/Post";
-import Blog404 from "./pages/blog/Blog404";
-import Page404 from "./pages/Page404";
-import Profile from "./pages/Profile";
-import PrivateRoute from "./components/PrivateRoute";
-import AuthLayout from "./pages/auth/AuthLayout";
-import Login from "./pages/auth/Login";
-import HomeLayout from "./pages/HomeLayout";
+import { useRoutes } from "react-router-dom";
+import routes from "./routes";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomeLayout />}>
-          <Route index={true} element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/blog" element={<BlogLayout />}>
-            <Route index={true} element={<Blog />} />
-            <Route path="categories" element={<Categories />} />
-            <Route path="post/:url" element={<Post />} />
-            <Route path="*" element={<Blog404 />} />
-          </Route>
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            }
-          />
-        </Route>
-        <Route path="/auth" element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-        </Route>
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    </>
-  );
+  return useRoutes(routes);
 }
 
 export default App;
---
+++ router/src/routes.js
@@ -0,0 +1,77 @@
+import { Routes, Route } from "react-router-dom";
+import Categories from "./pages/blog/Categories";
+import Post from "./pages/blog/Post";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import HomeLayout from "./pages/HomeLayout";
+import BlogLayout from "./pages/blog";
+import Blog from "./pages/blog/Blog";
+import Blog404 from "./pages/blog/Blog404";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./pages/Profile";
+import AuthLayout from "./pages/auth/AuthLayout";
+import Login from "./pages/auth/Login";
+import Page404 from "./pages/Page404";
+
+const routes = [
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "blog",
+        element: <BlogLayout />,
+        children: [
+          {
+            index: true,
+            element: <Blog />,
+          },
+          {
+            path: "categories",
+            element: <Categories />,
+          },
+          {
+            path: "post/:url",
+            element: <Post />,
+          },
+          {
+            path: "*",
+            element: <Blog404 />,
+          },
+        ],
+      },
+      {
+        path: "profile",
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: "auth",
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <Page404 />,
+  },
+];
+
+export default routes;
