346da6e5a2584689ae768cfd3efcb30a6ee8efa5
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-07-09 00:05:22 +0300

    edit: dynamic path

--- router/src/pages/blog/Blog.js
+++ router/src/pages/blog/Blog.js
@@ -1,15 +1,39 @@
+import { Link } from "react-router-dom";
+import { url } from "../../utils";
+
 export default function Blog() {
+  const posts = [
+    {
+      id: 1,
+      url: "post-1",
+      title: "Post 1",
+    },
+    {
+      id: 2,
+      url: "post-2",
+      title: "Post 2",
+    },
+    {
+      id: 3,
+      url: "post-3",
+      title: "Post 3",
+    },
+  ];
+
   return (
     <ul>
-      <li>
-        <a href="#">blog 1</a>
-      </li>
-      <li>
-        <a href="#">blog 2</a>
-      </li>
-      <li>
-        <a href="#">blog 3</a>
-      </li>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link
+            to={url("home.blog.post", {
+              id: post.id,
+              url: post.url,
+            })}
+          >
+            {post.title}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 }
