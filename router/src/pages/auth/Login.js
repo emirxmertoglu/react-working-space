6f44201461192aa033454c1a4fb3cded0a296d25
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-07-09 23:01:04 +0300

    update: login form with useFormik

--- router/src/pages/auth/Login.js
+++ router/src/pages/auth/Login.js
@@ -1,6 +1,7 @@
 import { useAuth } from "../../context/AuthContext";
 import { useNavigate, useLocation } from "react-router-dom";
 import { Helmet } from "react-helmet";
+import { useFormik } from "formik";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -8,19 +9,18 @@ export default function Login() {
 
   const { setUser } = useAuth();
 
-  const handleLogin = () => {
-    setUser({
-      id: 1,
-      username: "emirmertoglu",
-    });
-
-    navigate(location?.state?.return_url || "/", {
-      replace: true,
-      state: {
-        name: "emir",
-      },
-    });
-  };
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      setUser(values);
+      navigate(location?.state?.return_url || "/", {
+        replace: true,
+      });
+    },
+  });
 
   return (
     <>
@@ -30,7 +30,28 @@ export default function Login() {
       </Helmet>
 
       <h2>Login Page</h2>
-      <button onClick={handleLogin}>LogIn</button>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={values.username}
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+        <br />
+
+        <button type="submit">LogIn</button>
+      </form>
     </>
   );
 }
