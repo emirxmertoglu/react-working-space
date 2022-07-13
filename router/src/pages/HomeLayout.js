b38029b96f391ab8ac6235281d270f677697adc6
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-07-13 23:47:16 +0300

    fix: navigator ui

--- router/src/pages/HomeLayout.js
+++ router/src/pages/HomeLayout.js
@@ -1,14 +1,29 @@
 import { Outlet, NavLink } from "react-router-dom";
 import { url } from "../utils";
+import classNames from "classnames";
 
 export default function HomeLayout() {
+  const myClass = ({ isActive }) =>
+    classNames({
+      "px-1.5 mr-1.5 last:mr-0": true,
+      "bg-yellow-300 rounded": isActive,
+    });
+
   return (
     <>
-      <nav>
-        <NavLink to={url("home")}>Home</NavLink>
-        <NavLink to={url("home.contact")}>Contact</NavLink>
-        <NavLink to={url("home.blog")}>Blog</NavLink>
-        <NavLink to={url("home.profile")}>Profile</NavLink>
+      <nav className="mt-2 ml-2">
+        <NavLink to={url("home")} className={myClass}>
+          Home
+        </NavLink>
+        <NavLink to={url("home.contact")} className={myClass}>
+          Contact
+        </NavLink>
+        <NavLink to={url("home.blog")} className={myClass}>
+          Blog
+        </NavLink>
+        <NavLink to={url("home.profile")} className={myClass}>
+          Profile
+        </NavLink>
       </nav>
       <Outlet />
     </>
