ce60398c41cb6a7a7fdd3a4dd3458a5934d58a1f
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-07-28 22:55:43 +0300

    update: static path

--- nextjs-first-look/pages/posts/[id].js
+++ nextjs-first-look/pages/posts/[id].js
@@ -7,10 +7,29 @@ export default function PostDetails({ post }) {
   );
 }
 
-export const getServerSideProps = async (ctx) => {
+export const getStaticPaths = async () => {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts").then(
+    (res) => res.json()
+  );
+  const paths = posts.map((post) => {
+    return {
+      params: {
+        id: post.id.toString(),
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (ctx) => {
   const post = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${ctx.params.id}`
   ).then((res) => res.json());
+
   return {
     props: {
       post,
