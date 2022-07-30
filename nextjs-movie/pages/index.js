c320ff3d0fc0d3724ae612433d847ec410e3fdc1
emirxmertoglu <emir.mertoglu.97@gmail.com>
2022-07-30 13:06:55 +0300

    initialize commit

---
+++ nextjs-movie/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Test App</title>
+        <meta name="description" content="Test App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>Test App</div>
+    </div>
+  );
+}
